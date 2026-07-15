a7e32cd063cb13b3399a845615fe2edb0a13dffa
Mich-roan <michelle.rodriguez@uteca.edu.mx>
2026-07-14 20:51:35 -0600

    haciendo sniper

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -17,9 +17,11 @@ public class player : MonoBehaviour
     private Health health;
     private Rigidbody rb;
     public float CurrentHealth => health.CurrentHealth;
+    private FirstPersonMovement firstPersonMovement;
     private Gun currentGun;
     private void Awake()
     {
+        firstPersonMovement = GetComponent<FirstPersonMovement>();
         rb = GetComponent<Rigidbody>();
         health = GetComponent<Health>();
     }
@@ -60,7 +62,7 @@ public class player : MonoBehaviour
     public void PushBack(Transform enemy, float force)
     {
         Vector3 pushDirection = (transform.position - enemy.position).normalized;
-       rb.AddForce(pushDirection * force, ForceMode.Impulse);
+       firstPersonMovement.AddKnockback(pushDirection, force);
     }
     public void Die()
     {
